e9801750636e0357c7962db5bf65ccc4c31bc3ce
icejump <1639824820@qq.com>
2017-12-05 21:05:52 +0800

    f

--- mq/src/testsvn/Test.java
+++ mq/src/testsvn/Test.java
@@ -2,10 +2,5 @@ package testsvn;
 
 public class Test {
     public static void main(String[] args) {
-        int a=0;
-        int b=0;
-        int c=0;
-        int d=0;
-        int e=0;
     }
 }
